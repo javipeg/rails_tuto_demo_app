599858100e24b53c5837d30342e12d2b95821dc1
Javier Pérez <javipeg@gmail.com>
2013-02-02 17:31:47 +0100

    Initial commit. Rails tuto demo app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DemoAppRailstutorial::Application.config.secret_token = '68bf750e2665a544eb3225b4f8b8200a45602d45584f6e0c34c6f58b4825e9fbf4bf57d8ba8941e2ea2b91d57308f264b5896ad0b0238e1bd4f73aaaf2264542'
